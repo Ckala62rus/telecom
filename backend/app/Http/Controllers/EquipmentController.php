bd36a4e28ebe43942f64a0e08b9c8d7d1b7fb9bf
Овчинников Евгений Аркадьевич <eovchinnikov@rk.leather.ru>
2022-06-06 09:51:39 +0300

    fix

--- backend/app/Http/Controllers/EquipmentController.php
+++ backend/app/Http/Controllers/EquipmentController.php
@@ -8,7 +8,6 @@ use App\Http\Requests\Equipment\EquipmentUpdateRequest;
 use App\Http\Resources\Equipment\EquipmentAllResource;
 use App\Services\EquipmentService;
 use Illuminate\Http\JsonResponse;
-use Illuminate\Http\Request;
 
 class EquipmentController extends BaseController
 {
