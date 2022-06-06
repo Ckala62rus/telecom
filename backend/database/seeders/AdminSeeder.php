8c14e0125ecc23c3c339a3834cf9d907fd98b4de
Овчинников Евгений Аркадьевич <eovchinnikov@rk.leather.ru>
2022-06-06 10:56:08 +0300

    auth routes

---
+++ backend/database/seeders/AdminSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class AdminSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::create([
+            'name' => 'Owner',
+            'email' => 'owner@mail.ru',
+            'password' => Hash::make('123123123'),
+        ]);
+    }
+}
